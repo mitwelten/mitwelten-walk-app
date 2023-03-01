9d04754c51a876ffde8268cb7ec79c51891832d9
Cedric Spindler <cedric.spindler@gmail.com>
2023-03-01 09:01:26 +0100

    Rename app to Mitwelten Datawalk

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'datawalk-proto'`, () => {
+  it(`should have as title 'Mitwelten Datawalk'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('datawalk-proto');
+    expect(app.title).toEqual('Mitwelten Datawalk');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('datawalk-proto app is running!');
+    expect(compiled.querySelector('mat-toolbar span.mat-h2')?.textContent).toContain('Mitwelten Datawalk');
   });
 });
