9901e5912aeae2b90326ad202c1dc422bb964cac
Cedric Spindler <cedric.spindler@gmail.com>
2023-05-08 20:21:46 +0200

    Move map view to own route - Add navigation to switch between stack fade and map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StackFadeComponent } from './components/stack-fade/stack-fade.component';
 import { InfoComponent } from './components/info/info.component';
+import { MapComponent } from './components/map/map.component';
 
 const routes: Routes = [
   { path: 'info', component: InfoComponent },
+  { path: 'map', component: MapComponent },
   { path: '', component: StackFadeComponent, children: [] }
 ];
 
