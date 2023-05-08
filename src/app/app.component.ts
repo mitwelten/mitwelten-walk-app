639d859f33b4af0118342572bf1ad51fdb13677a
Cedric Spindler <cedric.spindler@gmail.com>
2023-05-08 22:12:21 +0200

    Drop rundant geolocation, prefer parcours service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
-import { GeolocationService } from '@ng-web-apis/geolocation';
-import { startWith } from 'rxjs';
 import { KeycloakProfile } from 'keycloak-js';
-import { CoordinatePoint, Deployment } from './shared';
-import { DataService, EntryService, OidcService, StateService, TrackProgressService, TrackRecorderService } from './services';
+import { Deployment } from './shared';
+import { DataService, EntryService, OidcService, ParcoursService, StateService, TrackProgressService, TrackRecorderService } from './services';
 import pkgJson from '../../package.json';
 
 @Component({
@@ -15,7 +13,6 @@ import pkgJson from '../../package.json';
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'Mitwelten Walk';
   version = pkgJson.version;
-  location?: CoordinatePoint;
   deployments: (Deployment & { distance: number })[] = [];
   showDeployments = false;
   isLoggedIn = false;
@@ -28,7 +25,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   private toggleDebugView!: MatSlideToggle;
 
   constructor(
-    private readonly geolocation: GeolocationService,
+    private parcoursService: ParcoursService, // initialise geolocation
     public trackRecorder: TrackRecorderService,
     public trackProgress: TrackProgressService,
     private entryService: EntryService,
@@ -41,17 +38,6 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.authService.authStateSubject.subscribe(state => this.isLoggedIn = state);
     this.authService.userData().subscribe(profile => this.userData = profile);
     this.authService.checkLogin();
-
-    this.geolocation.pipe(
-      startWith({ coords: { // reinacher heide
-        longitude: 7.609027254014222,
-        latitude: 47.506450512010844
-      }})
-      // tap(l => console.dir(l))
-      ).subscribe(l => {
-        const loc: CoordinatePoint = { lon: l.coords.longitude, lat: l.coords.latitude}
-        this.location = loc;
-    });
   }
 
   ngAfterViewInit() {
