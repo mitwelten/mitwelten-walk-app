92465ff66f02c8c07cc21c3ffaedfa91dee13457
Cedric Spindler <cedric.spindler@gmail.com>
2023-05-11 00:12:33 +0200

    Add UI to load GPS trace

--- src/app/components/record-control/record-control.component.ts
+++ src/app/components/record-control/record-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TrackRecorderService } from 'src/app/services';
 
 @Component({
@@ -13,11 +13,21 @@ import { TrackRecorderService } from 'src/app/services';
       <mat-icon color="accent" class="material-symbols-outlined">restart_alt</mat-icon>
       <span>Clear GPS trace</span>
     </button>
+    <div mat-menu-item disabled>GPS trace playback</div>
+    <button mat-menu-item (click)="fileInput.click()">
+      <mat-icon color="accent" class="material-symbols-outlined">upload</mat-icon>
+      <span>Load GPS trace</span>
+    </button>
+
+    <input #fileInput type="file" style="display:none" (change)="handleFile()">
   `
 })
 export class RecordControlComponent implements OnInit {
 
   public elems: number = 0;
+
+  @ViewChild('fileInput')
+  fileInput?: ElementRef<HTMLInputElement>;
 
   constructor(
     public trackRecorder: TrackRecorderService
@@ -26,4 +36,15 @@ export class RecordControlComponent implements OnInit {
   ngOnInit(): void {
     this.trackRecorder.track.subscribe(track => this.elems = track.length);
   }
+
+  handleFile(){
+    const reader = new FileReader();
+    reader.onload = (ev) => {
+      const track = <GeolocationPosition[]>JSON.parse(<string>ev.target?.result);
+    };
+    if (this.fileInput?.nativeElement.files?.length) {
+      reader.readAsText(this.fileInput?.nativeElement.files[0]);
+    }
+  }
+
 }
