61f2a245916725ebe9261800ceb5586378f68404
Cedric Spindler <cedric.spindler@gmail.com>
2023-10-21 13:43:14 +0200

    Update text for perimeter warning

--- src/app/components/distance-warning-dialog.component.ts
+++ src/app/components/distance-warning-dialog.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-distance-warning-dialog',
   template: `
-    <h1 mat-dialog-title>Warning</h1>
-    <div mat-dialog-content>You are too far away from the parcours. Progress tracking is now disabled.</div>
+    <h1 mat-dialog-title>Ausserhalb des Perimeters</h1>
+    <div mat-dialog-content>
+      <p>
+        Sie befinden sich ausserhalb des Gebietes der Reinacher Heide, für die diese WalkApp entwickelt wurde. Um die
+        WalkApp erleben zu können, müssen sie das Naturschutzgebiet vor Ort besuchen. Die Reinacher Heide befindet sich
+        in der Gemeinde Reinach, am Fluss “Birs”, unweit von Basel. Sie erreichen sie mit der Tram 10 oder der S-Bahn
+        S3, Station Bahnhof Dornach-Arlesheim, Fussweg ca. 10 Minuten.
+      </p>
+    </div>
     <div mat-dialog-actions>
       <button mat-button mat-dialog-close>Close</button>
     </div>
