b2fa211589332e2ec7e3ff3dbcd08176064ff342
Cedric Spindler <cedric.spindler@gmail.com>
2023-11-09 21:09:34 +0100

    Turn on audio notification when starting a walk

--- src/app/components/choose-channel/choose-channel.component.ts
+++ src/app/components/choose-channel/choose-channel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { OidcService } from 'src/app/services';
+import { AudioService } from 'src/app/services/audio.service';
 
 @Component({
   selector: 'app-choose-channel',
@@ -24,7 +25,8 @@ export class ChooseChannelComponent implements OnInit {
   constructor(
     private router: Router,
     public dialogRef: MatDialogRef<ChooseChannelComponent>,
-    public authService: OidcService
+    public authService: OidcService,
+    private audioService: AudioService,
   ) {
 
   }
@@ -34,7 +36,13 @@ export class ChooseChannelComponent implements OnInit {
     this.authService.checkLogin();
   }
 
+  /**
+   * Selects a channel (a.k.a. "Erlebnismodus") by its ID and navigates to the corresponding path.
+   * If the audio service is not running, it starts it.
+   * @param id The ID of the channel to select.
+   */
   select(id: number) {
+    this.audioService.running.getValue() || this.audioService.start();
     this.router.navigate([this.pathMap[id]]).then(() => {
       this.selection = id;
       this.dialogRef.close(this.selection);
