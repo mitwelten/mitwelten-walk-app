11442c4657f67b73f7f7ddb8b66102717456e206
Cedric Spindler <cedric.spindler@gmail.com>
2024-05-01 18:00:29 +0200

    Try checkLoginIframe: false for better login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -71,7 +71,8 @@ function initializeKeycloak(keycloak: KeycloakService) {
       initOptions: {
         onLoad: 'check-sso',
         silentCheckSsoRedirectUri:
-          window.location.origin + '/assets/silent-check-sso.html'
+          window.location.origin + '/assets/silent-check-sso.html',
+        checkLoginIframe: false
       }
     });
 }
