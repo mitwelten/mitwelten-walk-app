8efc0ad865d7cd3263fdf340ca029a143d971137
Cedric Spindler <cedric.spindler@gmail.com>
2024-05-01 18:54:06 +0200

    Update welcome and instructions

--- src/app/components/instructions.component.ts
+++ src/app/components/instructions.component.ts
@@ -4,26 +4,49 @@ import { Component } from '@angular/core';
   selector: 'app-instructions',
   template: `
 <p>
-  Für eine optimierte Bildschirmdarstellung installieren Sie die WalkApp bitte auf Ihrem Home-Screen (progressive Web-App). Eine Benutzung direkt im Browser ist möglich, aber die folgenden Einstellungen verbessern Ihr Ergebnis.
+  Die WalkApp funktioniert direkt im Browser unter folgender URL:
+  <a href="https://walk.mitwelten.org/" style="color: #ff4081">https://walk.mitwelten.org/</a>.
+  Sie ist location-sensitive (GPS-basiert) und kann nur vor Ort in der Reinacher Heide sinnvoll genutzt werden.
+  Sie können die Darstellung und das Handling der WalkApp verbessern, indem Sie folgende Einstellungen vornehmen.
+</p>
+
+<h4>Einrichtung eines Home-Screen oder Progressive Web-App (PWA)</h4>
+
+<p>
+  Je nach Browser oder verwendetem System sieht der Prozess etwas anders aus,
+  entspricht in etwa aber folgenden Schritten:
 </p>
 
 <ul>
-  <li>öffnen Sie walk.mitwelten.org in Ihrem Browser</li>
-  <li>klicken sie im Menu am unteren Bildschirmrand auf
-    <mat-icon inline="true" class="material-symbols-outlined">ios_share</mat-icon>, um ein neues Menu zu öffnen</li>
-  <li>scrollen Sie im Menu nach unten und wählen Sie “zum Home-Bildschirm hinzufügen” </li>
-  <li>es erscheint ein neues Mitwelten-Icon (rosarot mit einer Spirale) unter Ihren Apps</li>
-  <li>öffnen Sie die WalkApp mit diesem Icon </li>
+  <li>Öffnen Sie <a href="https://walk.mitwelten.org/" style="color: #ff4081">https://walk.mitwelten.org/</a> in Ihrem Browser.</li>
+  <li>Klicken sie im Menu am unteren Bildschirmrand (oder oben im Balken) auf das Symbol
+    <mat-icon inline="true" class="material-symbols-outlined">ios_share</mat-icon>, um ein neues Menu zu öffnen.</li>
+  <li>Scrollen Sie im Menu nach unten und wählen Sie “<em>Zum Home-Bildschirm</em>” (Alternativ kann die Option PWA erscheinen, die es zu installieren gilt).</li>
+  <li>Es erscheint ein neues Mitwelten-Icon (rosarot mit einer Spirale) unter Ihren Apps:
+    <div class="screenshot-container">
+      <div id="app-icon-crop">
+        <img  src="assets/app-launch-icon.webp" alt="App Icon">
+      </div>
+    </div>
+  </li>
+  <li>Öffnen Sie die WalkApp mit diesem Icon. Sie werden dann gefragt, ob die WalkApp auf ihre <strong>GPS-Signal</strong> zugreifen darf.
+  Bitte erteilen Sie die Erlaubnis (es werden keine persönlichen Daten erfasst).</li>
+  <li>
+  Falls der vorherige Schritt nicht geklappt hat oder Sie keine Informationen auf dem Mobile erhalten, kann der Prozess auch selbst ausgeführt werden:
+  Auf dem iPhone finden Sie unter <em>Einstellungen</em> > <em>Datenschutz & Sicherheit</em> > <em>Ortungsdienste</em>.
+  Bitte schalten Sie ein. Scrollen Sie dann zum Browser, den sie verwenden (Safari, Firefox, Chrome, …), und aktivieren sie das Häkchen „<em>Beim Verwenden der App</em>“.
+  </li>
 </ul>
 
-<p>
-  Erlauben Sie der WalkApp bitte den Zugriff auf Ihre Positionsdaten (es werden keine persönlichen Daten erfasst).
-</p>
+<h4>Weitere Einstellungen</h4>
 
-<p>
-  Aktivieren Sie bitte den Ton.
-</p>
+<ul>
+  <li>Aktivieren Sie bitte den Ton: Schalten Sie das Mobile auf laut und die Lautstärke relativ hoch.</li>
+  <li>Optional: Deaktivieren Sie bitte den Schlafmodus und nutzen Sie während des Spaziergangs keine anderen Funktionen ihres Mobiltelefons (respektive bringen Sie die WalkApp nach einer anderen Nutzung des Mobiles wieder in den Vordergrund). Die WalkApp bleibt somit auch aktiviert, wenn Sie den Bildschirm längere Zeit nicht berühren. Beim iPhone finde Sie das unter Einstellung > Anzeige & Helligkeit.  Automatische Sperre auf „Nie“ stellen. </li>
+  <li>Diese WalkApp ist eine prototypische Umsetzung des Forschungsprojektes MITWELTEN. Sollten Sie Probleme feststellen, lohnt es sich einen Restart vorzunehmen: WalkApp schliessen und wieder öffnen.</li>
+</ul>
 
+<!--
 <p>
   Deaktivieren Sie bitte den Schlafmodus und nutzen Sie während des Spaziergangs keine anderen Funktionen ihres
   Mobiltelefons (respektive bringen Sie die WalkApp nach Nutzung anderer Funktionen wieder als aktive App in den
@@ -50,10 +73,27 @@ import { Component } from '@angular/core';
 
 <p>
   <em>Wir wünschen Ihnen viel Spass!</em>
-</p>
+</p> -->
   `,
   styles: [
-    `mat-icon.mat-icon.mat-icon-inline { line-height: 1; }`
+    `mat-icon.mat-icon.mat-icon-inline { line-height: 1; }`,
+    `.screenshot-container {
+      position: relative;
+      margin: 0.8em 0;
+    }`,
+    `#app-icon-crop {
+      width: 50%;
+      height: 100px;
+      overflow: hidden;
+      border-radius: 9px;
+    }`,
+    `#app-icon-crop img {
+      object-fit: contain;
+      position: relative;
+      left: -11px;
+      top: -74px;
+      width: 400px;
+    }`,
   ]
 })
 export class InstructionsComponent {
